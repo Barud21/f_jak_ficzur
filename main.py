f99ea9009c123339298922cda42d56081bc06a22
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-04-26 19:03:42 +0200

    Zadanie 5 - first draft

--- main.py
+++ main.py
@@ -13,6 +13,73 @@ security = HTTPBasic()
 app.session_tokens = []
 templates = Jinja2Templates(directory="templates")
 
+
+app.counter = 0
+app.patients = []
+
+##############################################################
+# Zadanie 1
+##############################################################
+
+@app.get('/')
+def hello_world():
+    return {"message": "Hello World during the coronavirus pandemic!"}
+
+
+##############################################################
+# Zadanie 2
+##############################################################
+
+@app.get('/method')
+def return_get():
+    return {"method": "GET"}
+
+@app.post('/method')
+def return_post():
+    return {"method": "POST"}
+
+@app.put('/method')
+def return_put():
+    return {"method": "PUT"}
+
+@app.delete('/method')
+def return_delete():
+    return {"method": "DELETE"}
+
+
+##############################################################
+# Zadanie 3
+##############################################################
+
+class patient_data_rq(BaseModel):
+    name: str
+    surename: str
+
+
+class patient_data_resp(BaseModel):
+    id: int
+    patient: patient_data_rq
+
+
+@app.post('/patient')
+def patient_data(rq: patient_data_rq):
+    app.patients.append(rq)
+    app.counter += 1
+    return patient_data_resp(id=app.counter, patient=rq)
+
+
+##############################################################
+# Zadanie 4
+##############################################################
+
+@app.get('/patient/{pk}')
+def show_patient_data(pk: int):
+    if pk < len(app.patients):
+        return app.patients[pk]
+    else:
+        raise HTTPException(status_code=204, detail="Index not found")
+
+
 ##############################################################
 # Zadanie 1
 ##############################################################
@@ -21,6 +88,7 @@ templates = Jinja2Templates(directory="templates")
 def welcome(request: Request, session_token: str = Cookie(None)):
     if session_token not in app.session_tokens:
         raise HTTPException(status_code=401, detail="Unauthorized")
+    #return {"message": "Welcome"}
     return templates.TemplateResponse("item.html", {"request": request, "user": "trudnY"})
 
 @app.get('/')
@@ -41,17 +109,18 @@ def get_current_username(response: Response, credentials: HTTPBasicCredentials =
             detail="Incorrect login or password",
             headers={"WWW-Authenticate": "Basic"}
         )
-    session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}")).hexdigest()
+    session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}", encoding='utf8')).hexdigest()
     app.session_tokens.append(session_token)
     response.set_cookie(key="session_token", value=session_token)
-    # RedirectResponse(url="/welcome")
     response = RedirectResponse("/welcome")
     response.status_code = status.HTTP_302_FOUND
     return response
 
-@app.get("/users/me")
-def read_current_user(username: str = Depends(get_current_username)):
-    return {"username": username}
+@app.get("/data")
+def create_cookie(*, response: Response, session_token: str = Cookie(None)):
+    if session_token not in app.session_tokens:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    response.set_cookie(key="session_token", value=session_token)
 ##############################################################
 # Zadanie 3
 ##############################################################
@@ -63,3 +132,39 @@ def logout(*, response: Response, session_token: str = Cookie(None)):
     app.session_tokens.remove(session_token)
     response = RedirectResponse("/")
     return response
+
+##############################################################
+# Zadanie 3
+##############################################################
+
+@app.post("/patient")
+def add_patient(*, response: Response, patient: patient_data_rq, session_token: str = Cookie(None)):
+    if session_token not in app.session_tokens:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    if app.counter > len(app.patients):
+        app.patients.append(patient)
+        app.counter =+ 1
+    response.set_cookie(key="session_token", value=session_token)
+    response = RedirectResponse(f"/patient/{app.counter-1}")
+    response.status_code = status.HTTP_302_FOUND
+
+@app.get("/patient")
+def show_patients(response: Response, session_token: str = Cookie(None)):
+    if session_token not in app.session_tokens:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    return app.patients
+
+@app.get("/patients/{id}")
+def show_patient_data(response: Response, id: int, session_token: str = Cookie(None)):
+    if session_token not in app.session_tokens:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    response.set_cookie(key="session_token", value=session_token)
+    if id < len(app.patients):
+        return app.patients[id]
+
+@app.delete("patient/{id}")
+def delete_patient(response: Response, id: int, session_token: str = Cookie(None)):
+    if session_token not in app.session_tokens:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    app.patients.remove(id)
+    response.status_code = status.HTTP_204_NO_CONTENT
